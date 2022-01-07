960dde7baaea85ff89242edbe8d5692d5beac384
Julio Solis <js@juliosolis.com>
2022-01-07 12:24:55 -0600

    Update signature verification

--- src/Message/Responses/AuthorizeResponse.php
+++ src/Message/Responses/AuthorizeResponse.php
@@ -45,8 +45,8 @@ class AuthorizeResponse extends AbstractResponse
             $signature .= $this->request->getMerchantId();
             $signature .= $this->request->getAcquirerId();
             $signature .= $this->request->getTransactionId();
-            $signature .= $this->request->formatAmount();
-            $signature .= $this->request->getCurrencyNumeric();
+//            $signature .= $this->request->formatAmount();
+//            $signature .= $this->request->getCurrencyNumeric();
 
             $signature = base64_encode(sha1($signature, true));
 
